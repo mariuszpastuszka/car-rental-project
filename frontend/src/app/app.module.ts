4ba1e36f92682cad28511a4cb44e82abfbe6ccf2
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-07-02 09:22:47 +0200

    clients component created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './components/app-component/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClientsComponent } from './components/clients/clients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientsComponent
   ],
   imports: [
     BrowserModule,
