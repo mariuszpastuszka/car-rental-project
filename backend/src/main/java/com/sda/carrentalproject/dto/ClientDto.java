1ad77b3ef9444088539b0db8a2052b05f263090b
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-07-02 10:25:07 +0200

    frontend client interface created

--- backend/src/main/java/com/sda/carrentalproject/dto/ClientDto.java
+++ backend/src/main/java/com/sda/carrentalproject/dto/ClientDto.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 
+// TODO: add nulls and not nulls annotations
 @Builder
 public record ClientDto(Long id,
 
---
+++ frontend/src/app/models/client.ts
@@ -0,0 +1,19 @@
+export interface Client {
+  id: number;
+
+  name: string;
+
+  surname: string;
+
+  phone: string;
+
+  email: string;
+
+  address: string | null;
+
+  hasDrivingLicense: boolean;
+
+  registrationDateTime: string;
+
+  dateOfBirth: string
+}
