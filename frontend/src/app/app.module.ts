92fbe400f734677449f2b1c04840e97a0f1c3fd2
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-07-08 09:30:45 +0200

    Application top bar created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,11 +9,13 @@ import {MatTableModule} from "@angular/material/table";
 import {MatSortModule} from "@angular/material/sort";
 import {MatPaginatorModule} from "@angular/material/paginator";
 import {HttpClientModule} from "@angular/common/http";
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientsComponent
+    ClientsComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
