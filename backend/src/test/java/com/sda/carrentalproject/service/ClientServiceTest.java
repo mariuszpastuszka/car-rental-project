ab487dbd35fb0aba6867b2773042754e68f21c4d
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-07-08 12:25:13 +0200

    creatingNewClient() test method for ClientService

--- backend/src/test/java/com/sda/carrentalproject/service/ClientServiceTest.java
+++ backend/src/test/java/com/sda/carrentalproject/service/ClientServiceTest.java
@@ -1,11 +1,15 @@
 package com.sda.carrentalproject.service;
 
+import com.sda.carrentalproject.domain.Client;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.Month;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +22,31 @@ class ClientServiceTest {
     @Test
     void checkInjectedService() {
         Assertions.assertNotNull(clientService, "Bean should be injected!");
+    }
+
+    @Test
+    void creatingNewClient() {
+        // create new client
+        Client client = Client.builder()
+                .name("mark")
+                .surname("t.")
+                .phone("555-123-555")
+                .email("markathome@gmail.com")
+                .address("uk")
+                .dateOfBirth(LocalDate.of(1990, Month.JANUARY, 7))
+                .build();
+        // save it
+        var saved = clientService.saveClient(client);
+
+        // verify it after saving
+        Assertions.assertNotNull(saved.getId(), "Entity should have id!");
+        Assertions.assertEquals(client.getName(), saved.getName());
+        Assertions.assertEquals(client.getSurname(), saved.getSurname());
+        Assertions.assertEquals(client.getPhone(), saved.getPhone());
+        Assertions.assertEquals(client.getEmail(), saved.getEmail());
+        Assertions.assertEquals(client.getAddress(), saved.getAddress());
+        Assertions.assertEquals(client.getDateOfBirth(), saved.getDateOfBirth());
+        Assertions.assertEquals(client.getRegistrationDateTime(), saved.getRegistrationDateTime());
     }
 
     @BeforeEach
