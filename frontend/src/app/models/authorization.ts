18cc45381a11f949b842f77152e69770a28123f4
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-07-16 10:57:14 +0200

    Adding LoginService for angular application

---
+++ frontend/src/app/models/authorization.ts
@@ -0,0 +1,6 @@
+export const emptyRole = '';
+export const adminRole = 'ADMIN';
+export const userRole = 'USER';
+export const defaultUserLogin = '';
+export const defaultUserName = 'Anonymous';
+export const defaultUserSurname = 'Anonymous';
---
+++ frontend/src/app/services/login-service/login.service.ts
@@ -0,0 +1,43 @@
+import {Injectable} from '@angular/core';
+import {delay, Observable, of, tap} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {adminRole, defaultUserLogin, defaultUserName, defaultUserSurname, emptyRole} from "../../models/authorization";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  isLoggedIn = false;
+  userRole = emptyRole;
+  name = defaultUserName;
+  surname = defaultUserSurname;
+  userLogin = defaultUserLogin;
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  login(userLogin: string, pass: string): Observable<boolean> {
+    return of(true)
+      .pipe(
+        delay(2000),
+        tap(value => {
+          this.isLoggedIn = true;
+          this.userRole = adminRole;
+          this.userLogin = userLogin;
+          this.name = 'Maniek';
+          this.surname = 'P.';
+        })
+      )
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+    this.userRole = emptyRole;
+    this.name = defaultUserName;
+    this.surname = defaultUserSurname;
+    this.userLogin = defaultUserLogin;
+  }
+}
