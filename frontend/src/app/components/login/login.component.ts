922052f5497caf5b4e102f2549a3f019194842eb
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-07-16 12:06:41 +0200

    Login component - in progress still ;)

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {LoginService} from "../../services/login-service/login.service";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(
+    public loginService: LoginService
+  ) {
+  }
 
+  getStatusMessage(): string {
+    let message = '';
+    if (this.loginService.isLoggedIn) {
+      message = `Welcome ${this.loginService.name} ${this.loginService.surname}. Don't log out:)`
+    } else {
+      message = 'Please log in'
+    }
+    return message;
+  }
 }
