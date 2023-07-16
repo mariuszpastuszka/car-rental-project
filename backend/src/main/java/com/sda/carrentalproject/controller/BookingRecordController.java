0e78baac7b96c48f03c3098f4552f93960625b94
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-07-16 09:58:00 +0200

    Endpoint /booking-records completed

--- backend/src/main/java/com/sda/carrentalproject/controller/BookingRecordController.java
+++ backend/src/main/java/com/sda/carrentalproject/controller/BookingRecordController.java
@@ -1,14 +1,19 @@
 package com.sda.carrentalproject.controller;
 
+import com.sda.carrentalproject.domain.BookingRecord;
 import com.sda.carrentalproject.dto.BookingRecordDto;
 import com.sda.carrentalproject.dto.CarBookingRequestDto;
 import com.sda.carrentalproject.mapper.BookingRecordMapper;
 import com.sda.carrentalproject.service.BookingRecordService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -34,7 +39,15 @@ public class BookingRecordController {
     }
 
     @PostMapping("/booking-records")
-    public BookingRecordDto bookCar(@RequestBody CarBookingRequestDto carBookingRequest) {
-        return null;
+    public ResponseEntity<BookingRecordDto> bookCar(@RequestBody CarBookingRequestDto carBookingRequest,
+                                                    UriComponentsBuilder ucb) {
+        log.info("trying to book car with arguments: [{}]", carBookingRequest);
+        BookingRecord saved = bookingRecordService.createNewBooking(carBookingRequest);
+        URI path = ucb.path("/api/booking-records/{id}")
+                .buildAndExpand(Map.of("id", saved.getId()))
+                .toUri();
+
+        return ResponseEntity.created(path)
+                .body(mapper.fromEntityToDto(saved));
     }
 }
