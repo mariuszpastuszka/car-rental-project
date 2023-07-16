4222bdb38caac9bbd71981682b9d924ce95aefbc
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-07-16 12:42:50 +0200

    Login component - with buttons - without form

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {LoginService} from "../../services/login-service/login.service";
+import {Router} from "@angular/router";
+import {homePageUrl} from "../../models/links";
 
 @Component({
   selector: 'app-login',
@@ -7,18 +9,52 @@ import {LoginService} from "../../services/login-service/login.service";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  message! : string;
+  isLoggedIn!: boolean;
+  isLoggedOut! :boolean;
+
   constructor(
-    public loginService: LoginService
+    private loginService: LoginService,
+    private router: Router
   ) {
+    this.updateStatus();
   }
 
   getStatusMessage(): string {
-    let message = '';
+    let message
     if (this.loginService.isLoggedIn) {
       message = `Welcome ${this.loginService.name} ${this.loginService.surname}. Don't log out:)`
     } else {
       message = 'Please log in'
     }
     return message;
+  }
+  // TODO: fix navigation link activity
+  login() {
+    console.log('inside login method()');
+
+    this.message = "Trying to login....";
+    this.loginService
+      .login('me', 'you')
+      .subscribe(value => {
+        this.updateStatus();
+        this.router.navigateByUrl(homePageUrl)
+          .then();
+      })
+  }
+
+  logout() {
+    console.log('inside logout method()');
+    this.loginService.logout();
+    this.updateStatus();
+    this.router.navigateByUrl(homePageUrl)
+      .then();
+  }
+
+  private updateStatus() {
+    this.message = this.getStatusMessage();
+    this.isLoggedIn = this.loginService.isLoggedIn;
+    this.isLoggedOut = !this.loginService.isLoggedIn;
   }
 }
