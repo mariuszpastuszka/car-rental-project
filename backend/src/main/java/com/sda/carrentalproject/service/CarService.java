c100f1aaa435b5cfd634190c98ee8f45cc2c41b8
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-07-16 11:00:21 +0200

    fixes after merge

--- backend/src/main/java/com/sda/carrentalproject/service/CarService.java
+++ backend/src/main/java/com/sda/carrentalproject/service/CarService.java
@@ -6,9 +6,6 @@ import com.sda.carrentalproject.repository.CarRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Stream;
-
 @Service
 @Slf4j
 public class CarService {
